6223357e594a11dbd8f02db074f9f086e3914c42
polash <34005640+sshahriazz@users.noreply.github.com>
2023-11-22 11:38:42 +0600

    init

---
+++ devops/test/devops.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Devops from '../lib/devops-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devops-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Devops.DevopsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
